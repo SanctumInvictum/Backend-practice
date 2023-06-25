575766f178cd10999b2c6ab2844562cbde8cd2d9
SanctumInvictum <yura.sokolov.03@gmail.com>
2023-06-25 05:04:16 +0300

    add kafka, add HTTPException

--- src/Converter/converter.py
+++ src/Converter/converter.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-#from ..DataBase import get_async_session
-#from models import conversion
-#from schemas import ConversionCreate
+from DataBase import get_async_session
+from Converter.models import conversion
+from Converter.schemas import ConversionCreate
 
 router = APIRouter(
     prefix="/converter",
--- src/main.py
+++ src/main.py
@@ -1,41 +1,61 @@
-from fastapi import FastAPI, UploadFile, Depends
+from fastapi import FastAPI, UploadFile, Depends, HTTPException
 import s3_Yandex as s3
 from models import states
-from sqlalchemy import select, insert
+from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from DataBase import get_async_session
-from Converter.converter import router
+from Converter import converter as converter_router
+from Kafka import router as kafka_router
 import uuid
+
 
 # Создаем экземляр приложения
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(converter_router.router)
+app.include_router(kafka_router.route)
+
+
 # Эндпоинт с POST-запросом для прикрепления файла
 @app.post("/file/upload-file")
 async def upload_file(file: UploadFile, final_ext: str, session: AsyncSession = Depends(get_async_session)):
     file_id = str(uuid.uuid4())
-    stmt = insert(states).values(id=file_id, file_name=file.filename, start_extension=file.filename.split('.')[-1], final_extension=final_ext, state=0)
-    await session.execute(stmt)
-    await session.commit()
-    s3.upload(file=file.file, object_name='upload/'+file.filename)
-    return {"status": 200, "data": file_id}
+    try:
+        stmt = insert(states).values(id=file_id, file_name=file.filename, start_extension=file.filename.split('.')[-1], final_extension=final_ext, state=0)
+        await session.execute(stmt)
+        await session.commit()
+        s3.upload(file=file.file, object_name='upload/'+file.filename)
+        kafkaProducer.produce_data(topic='test-topic2', data=file_id)
+        return {"status": 200, "data": file_id}
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
+
 
 # uvicorn main:app --reload
 # Эндпоинт с GET-запросом для чтения данных о состоянии конвертации файла
 @app.get("/check/{file_id}")
 async def check_state(file_id: str, session: AsyncSession = Depends(get_async_session)):
-    query = states.select().where(states.c.id == file_id)
-    result = await session.execute(query)
-    status = result.fetchone()[-1]
-    match status:
-        case 0:
-            return {'status': 200, 'response': "Файл успешно загружен"}
-        case 1:
-            return {'status': 200, 'response': "Идет обработка"}
-        case _:
-            return {'status': 500, 'response': "Неверное состояние файла"}
-
+    try:
+        query = states.select().where(states.c.id == file_id)
+        result = await session.execute(query)
+        status = result.fetchone()[-1]
+        match status:
+            case 0:
+                return {'status': 200, 'response': "Файл успешно загружен"}
+            case 1:
+                return {'status': 200, 'response': "Идет обработка"}
+            case _:
+                return {'status': 500, 'response': "Неверное состояние файла"}
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
 
 # Эндпоинт с GET-запросом для получения переконвертированного файла из S3
 @app.get("/download/{file_name}")
